cc66d34e52cdf0c7f653e2c3276988c5576d7c48
sagar <sagarslife222@gmail.com>
2025-10-16 23:28:29 +0530

    SENTRY ERROR SETUP

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,22 +1,56 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from "react";
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { ClerkProvider } from '@clerk/clerk-react'
-import { BrowserRouter } from 'react-router'
+import {
+  Routes,
+  Route,
+  BrowserRouter,
+  useLocation,
+  useNavigationType,
+  createRoutesFromChildren,
+  matchRoutes,
+} from "react-router";import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "react-hot-toast";
+import AuthProvider from './providers/AuthProvider.jsx';
+import * as Sentry from "@sentry/react";
+
+
+const queryClient = new QueryClient({});
+
+
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
 if (!PUBLISHABLE_KEY) {
   throw new Error('Missing Publishable Key')
 }
+Sentry.init({
+  dsn: "https://47a934a8d0a1fc848b80ebc7bcd76b6b@o4510153252732928.ingest.us.sentry.io/4510200407785472",
+  integrations: [
+    Sentry.reactRouterV7BrowserTracingIntegration({
+      useEffect: React.useEffect,
+      useLocation,
+      useNavigationType,
+      createRoutesFromChildren,
+      matchRoutes,
+    }),
+  ],
+  tracesSampleRate: 1.0,
+});
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
       <BrowserRouter>
 
-      
-      <App />
+
+        <QueryClientProvider client={queryClient}>
+          <AuthProvider>
+            <App />
+          </AuthProvider>
+          <Toaster position="top-right" />
+        </QueryClientProvider>
       </BrowserRouter>
     </ClerkProvider>
   </StrictMode>
