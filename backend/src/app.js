0c7e8a58bc9cc3af66657ca6899a250b60330a9c
sagar <sagarslife222@gmail.com>
2026-02-14 22:27:12 +0530

    .

--- backend/src/app.js
+++ backend/src/app.js
@@ -15,7 +15,10 @@ import cors from "cors";
 
 const app = express();
 app.use(express.json());
-app.use(cors({origin: "http://localhost:5173", credentials: true }));
+app.use(cors({
+  origin: "*",
+  credentials: true
+}));
 
 app.use(clerkMiddleware());
 app.get("/debug-sentry", (req, res) => {
